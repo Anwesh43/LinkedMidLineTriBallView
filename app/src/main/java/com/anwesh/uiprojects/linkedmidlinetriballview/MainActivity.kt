d552de5bf798c1c0dbed51fcbbbbf7218ccd522f
Anwesh43 <anweshthecool0@gmail.com>
2019-07-16 20:24:51 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmidlinetriballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmidlinetriballview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedmidlinetriballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.midlinetriballview.MidTriLineBallView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MidTriLineBallView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
