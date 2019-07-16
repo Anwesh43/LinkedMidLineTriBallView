5927d04a90acbf588707a37f3c88c7b5bfc4c30c
Anwesh43 <anweshthecool0@gmail.com>
2019-07-16 19:13:36 +0530

    adding MidTriLineBallView class

--- midlinetriballview/src/main/java/com/anwesh/uiprojects/midlinetriballview/MidTriLineBallView.kt
+++ midlinetriballview/src/main/java/com/anwesh/uiprojects/midlinetriballview/MidTriLineBallView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawMLTBNode(i : Int, scale : Float, paint : Paint) {
     drawMidLineTri(sc1, size, paint)
     restore()
 }
+
+class MidTriLineBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
