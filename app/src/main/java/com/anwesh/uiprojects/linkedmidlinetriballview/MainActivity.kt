29aef6f7d22e597fac7a63a7180dbade9f4e0a86
Anwesh43 <anweshthecool0@gmail.com>
2019-07-16 19:48:06 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedmidlinetriballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmidlinetriballview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedmidlinetriballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.midlinetriballview.MidTriLineBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MidTriLineBallView.create(this)
     }
 }
