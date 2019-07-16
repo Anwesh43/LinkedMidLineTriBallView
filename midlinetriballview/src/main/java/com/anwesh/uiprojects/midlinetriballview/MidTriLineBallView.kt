19ed141b0823861022670052682607bdb692e99d
Anwesh43 <anweshthecool0@gmail.com>
2019-07-16 19:41:36 +0530

    adding modtrilineballview module

--- midlinetriballview/src/main/java/com/anwesh/uiprojects/midlinetriballview/MidTriLineBallView.kt
+++ midlinetriballview/src/main/java/com/anwesh/uiprojects/midlinetriballview/MidTriLineBallView.kt
@@ -214,4 +214,13 @@ class MidTriLineBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : MidTriLineBallView {
+            val view : MidTriLineBallView = MidTriLineBallView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
